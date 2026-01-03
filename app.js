1507d5224e2e661a3f97db75ca449b4439c04153
Andi Marsituru Pakke <andimarsituru0064@gmail.com>
2026-01-03 15:51:50 +0800

    Cek Kondisi Berlapis Lebih Aman

--- app.js
+++ app.js
@@ -15,15 +15,15 @@
 //     console.log("Angka lebih kecil dari 0.5");
 // }
 
-const iniHari = prompt('kamu sayang aku ga').toLowerCase();
+// const iniHari = prompt('kamu sayang aku ga').toLowerCase();
 
-if(iniHari === 'iya') {
-    alert("aku juga sayang kamu :)")
-} else if (iniHari === 'ga') {
-    alert('jawab iya napa');
-} else {
-    alert('jawab iya atau ga');
-}
+// if(iniHari === 'iya') {
+//     alert("aku juga sayang kamu :)")
+// } else if (iniHari === 'ga') {
+//     alert('jawab iya napa');
+// } else {
+//     alert('jawab iya atau ga');
+// }
 
 // const nilai = '40';
 
@@ -38,3 +38,16 @@ if(iniHari === 'iya') {
 // } else if (nilai < 100) {
 //     console.log('A');
 // }
+
+const password = prompt("buat password");
+
+// Password harus sepanjang 6 karakter
+if(password.length >= 6) {
+    if(password.indexOf(' ') === -1) {
+    console.log("Password valid")
+} else {
+    console.log("Password tidak boleh ada spasi");
+}
+} else {
+    console.log("Password harus minimal 6 karakter");
+}
