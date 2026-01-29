07aa31cfa89c238d6b6a72f68d4b81e7b8ff096d
Andi <andimarsituru0064@gmail.com>
2026-01-29 13:18:25 +0800

    Contoh Lain Perulangan For

--- forLoop.js
+++ forLoop.js
@@ -1,4 +1,19 @@
-for (let i = 1; i <= 10; i++) {
+// for (let i = 1; i <= 10; i++) {
+//     console.log('saya ada di dalam loop ');
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log('saya ada di dalam loop ');
+//     console.log(i);
+// }
+
+// for (let i = 20; i >= 0; i -= 2) {
+//     console.log('saya ada di dalam loop ');
+//     console.log(i);
+// }
+
+for (let i = 10; i <= 100000; i *= 10) {
     console.log('saya ada di dalam loop ');
     console.log(i);
 }
