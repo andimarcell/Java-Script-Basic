b4a5eb7b669ebe96c572ab1795550c02a393abb2
Andi <andimarsituru0064@gmail.com>
2026-01-29 13:41:49 +0800

    Mendapatkan Data Dari Array Dengan Perulangan

--- forLoop.js
+++ forLoop.js
@@ -13,7 +13,17 @@
 //     console.log(i);
 // }
 
-for (let i = 10; i <= 100000; i *= 10) {
-    console.log('saya ada di dalam loop ');
-    console.log(i);
+// for (let i = 10; i <= 100000; i *= 10) {
+//     console.log('saya ada di dalam loop ');
+//     console.log(i);
+// }
+
+// for (let i = 20; i >= 0; i++) {
+//     console.log(i);
+// }
+
+const hewan = ['kucing', 'anjing', 'burung', 'ikan', 'hamster'];
+
+for (let i = 0; i < hewan.length; i++) {
+    console.log(i+1, hewan[i]); 
 }
