6879ec6cfc183bf7dda57cf591bdad6c2f298486
Andi <andimarsituru0064@gmail.com>
2026-02-03 14:31:48 +0800

    Mendefinisikan Dan Menjalankan Function

--- function.js
+++ function.js
@@ -5,3 +5,14 @@ function lemparDadu() {
     console.log(Math.floor(Math.random() * 6) + 1);
     
 }
+
+function nyanyi() {
+    console.log('la la la la la');
+    console.log('do re mi fa so la ti do');
+}
+
+nyanyi();
+nyanyi();
+nyanyi();
+nyanyi();
+nyanyi();
