fbefd3eac03603039936092e3067ee1586d1442f
Andi <andimarsituru0064@gmail.com>
2026-02-06 16:40:17 +0800

    Map Method

--- callbackFunction,MethodMilikArray.js
+++ callbackFunction,MethodMilikArray.js
@@ -1,5 +1,14 @@
-const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const angka = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const angkaDouble = angka.map(function(num) {
+    return num * 2;
+});
 
+const angkaMap = angka.map(function(number) {
+    // if(number * 2 === 0) {
+    //     console.log(number);
+    // }
+   return number * 2;
+});
 // angka.forEach(function (el) {
 //     if(el % 2 === 0) {
 //         console.log(el);
@@ -28,6 +37,33 @@ const animes = [
     }
 ];
 
-animes.forEach(function(anime) {
-    console.log(`${anime.title} - Rating: ${anime.rating}/100`);
-});
+// animes.forEach(function(anime) {
+//     console.log(`${anime.title} - Rating: ${anime.rating}/100`);
+// });
+// const animeList = animes.map(function(anime) {
+//     return anime.title.toUpperCase();
+// });
+// const animeRating = animes.map(function(anime) {
+//     return anime.rating / 10;
+// });
+
+// const animeInfo = animes.map(function(anime) {
+//     const titleUpper = anime.title.toUpperCase();
+//     const ratingDecimal = anime.rating / 10;
+//     return `${titleUpper} - Rating: ${ratingDecimal}/10`;
+// });
+
+// const animeData = animes.map(function(anime) {
+//     return {
+//         title: anime.title.toUpperCase(),
+//         rating: anime.rating / 10,
+//     };
+// });
+// console.log(animeData);
+
+const animeInfo = animes.map(anime => `${anime.title.toUpperCase()} - Rating: ${anime.rating / 10}/10`);
+
+const animeData = animes.map(anime => ({
+    title: anime.title.toUpperCase(),
+    rating: anime.rating / 10,
+}));
