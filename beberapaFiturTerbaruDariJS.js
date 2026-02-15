b077096a4b09565d0d42c2f77eba081d240aaae3
Andi <andimarsituru0064@gmail.com>
2026-02-15 23:14:08 +0800

    Mengubah Array atau Object Menjadi Deret Value Argument Function

--- beberapaFiturTerbaruDariJS.js
+++ beberapaFiturTerbaruDariJS.js
@@ -1,7 +1,10 @@
-function lemparDadu(sisi) {
-    return Math.floor(Math.random() * sisi) + 1;
-}
+// function lemparDadu(sisi) {
+//     return Math.floor(Math.random() * sisi) + 1;
+// }
 
-function sapa (name, msg ='uhuk') {
-    console.log(`${msg} ${name}`)
-}
+// function sapa (name, msg ='uhuk') {
+//     console.log(`${msg} ${name}`)
+// }
+
+const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+Math.max(...angka) // 10
