6548312a315282dd3e9b1ba773fcfe1d17360016
Andi <andimarsituru0064@gmail.com>
2026-02-15 23:14:57 +0800

    Menggabungkan Nilai Array Dengan Array Lagi

--- beberapaFiturTerbaruDariJS.js
+++ beberapaFiturTerbaruDariJS.js
@@ -6,5 +6,11 @@
 //     console.log(`${msg} ${name}`)
 // }
 
+// const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Math.max(...angka) // 10
+
 const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-Math.max(...angka) // 10
+const nama = ['Andi', 'Budi', 'Caca', 'Dedi', 'Eka'];
+
+
+const campuran = [...angka, 0, 'a', 'b'];
